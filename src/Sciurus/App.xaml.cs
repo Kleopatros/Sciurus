452093fc47f99a42ced19840ceb05ad7b1c15213
Kleopatros <47168369+Kleopatros@users.noreply.github.com>
2020-06-30 21:41:47 -0700

    Organize shell.

--- src/Sciurus/App.xaml.cs
+++ src/Sciurus/App.xaml.cs
@@ -1,17 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using Sciurus.Views;
 
 namespace Sciurus
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Interaction logic for the application.
     /// </summary>
+    /// <remarks>
+    /// Implementation based on PrismApplicationBase.
+    /// </remarks>
+    /// <seealso cref="https://github.com/PrismLibrary/Prism"/>
     public partial class App : Application
     {
+        /// <summary>
+        /// Raises the <see cref="Application.Startup"/> event.
+        /// </summary>
+        /// <param name="e">A <see cref="StartupEventArgs"/> that contains the event data.</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            this.Initialize();
+        }
+
+        /// <summary>
+        /// Runs the initialization sequence to configure the application.
+        /// </summary>
+        private void Initialize()
+        {
+            Window shell = this.CreateShell();
+            if (shell != null)
+            {
+                this.InitializeShell(shell);
+            }
+
+            this.OnInitialized();
+        }
+
+        /// <summary>
+        /// Creates the shell or main window of the application.
+        /// </summary>
+        /// <returns>The shell of the application.</returns>
+        private Window CreateShell()
+        {
+            return new Shell();
+        }
+
+        /// <summary>
+        /// Initializes the shell.
+        /// </summary>
+        private void InitializeShell(Window shell)
+        {
+            this.MainWindow = shell;
+        }
+
+        /// <summary>
+        /// Contains actions that should occur last in the initialization sequence.
+        /// </summary>
+        private void OnInitialized()
+        {
+            this.MainWindow?.Show();
+        }
     }
 }
---
+++ src/Sciurus/Views/Shell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Sciurus.Views
+{
+    /// <summary>
+    /// Interaction logic for the application shell.
+    /// </summary>
+    public partial class Shell : Window
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
