69a261ee986f0c6085318536b64401b878457048
Kleopatros <47168369+Kleopatros@users.noreply.github.com>
2020-07-24 16:45:25 -0700

    Test ViewModel.

---
+++ src/SciurusTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SciurusTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SciurusTests")]
+[assembly: AssemblyCopyright("Copyright © Kleopatros 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85884678-f8cd-47ed-993d-68a1cea2aead")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/SciurusTests/ViewModels/ViewModelTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sciurus.ViewModels;
+
+namespace SciurusTests
+{
+    /// <summary>
+    /// Tests the <see cref="ViewModel"/> class.
+    /// </summary>
+    [TestClass]
+    public class ViewModelTests
+    {
+        /// <summary>
+        /// Tests <see cref="ViewModel.Set{T}(ref T, T, string)"/> raises a
+        /// <see cref="System.ComponentModel.INotifyPropertyChanged.PropertyChanged"/> event when
+        /// the property value is changed.
+        /// </summary>
+        [TestMethod]
+        public void SetRaisesPropertyChanged()
+        {
+            TestViewModel test = new TestViewModel();
+            test.PropertyChanged += (sender, args) =>
+            {
+                Assert.AreSame(test, sender);
+                Assert.AreEqual(nameof(TestViewModel.IntProperty), args.PropertyName);
+            };
+
+            test.IntProperty = 5;
+        }
+
+        /// <summary>
+        /// Tests <see cref="ViewModel.Set{T}(ref T, T, string)"/> does not raise a
+        /// <see cref="System.ComponentModel.INotifyPropertyChanged.PropertyChanged"/> event when
+        /// the property value is set to the same value repeatedly.
+        /// </summary>
+        [TestMethod]
+        public void SetRaisesPropertyChangedOnlyIfChanged()
+        {
+            int eventCount = 0;
+
+            TestViewModel test = new TestViewModel();
+            test.PropertyChanged += (sender, args) => eventCount++;
+
+            test.IntProperty = 5;
+            Assert.AreEqual(1, eventCount);
+
+            test.IntProperty = 5;
+            Assert.AreEqual(1, eventCount);
+
+            test.IntProperty = 6;
+            Assert.AreEqual(2, eventCount);
+        }
+
+        /// <summary>
+        /// Tests <see cref="ViewModel.Get{T}(ref T)"/> returns the property value.
+        /// </summary>
+        [TestMethod]
+        public void GetReturnsValue()
+        {
+            TestViewModel test = new TestViewModel();
+            test.IntProperty = 5;
+            Assert.AreEqual(5, test.IntProperty);
+        }
+
+        /// <summary>
+        /// Facilitates testing of the <see cref="ViewModel"/> class, which is abstract.
+        /// </summary>
+        public class TestViewModel : ViewModel
+        {
+            private int _intProperty;
+
+            /// <summary>
+            /// Gets or sets an integer test property.
+            /// </summary>
+            public int IntProperty
+            {
+                get
+                {
+                    return this.Get(ref _intProperty);
+                }
+
+                set
+                {
+                    this.Set(ref _intProperty, value);
+                }
+            }
+        }
+    }
+}
