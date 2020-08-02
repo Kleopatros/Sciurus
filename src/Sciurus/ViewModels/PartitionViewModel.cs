06e91878f87aec917547cbcd758c52b6aab94a55
Kleopatros <47168369+Kleopatros@users.noreply.github.com>
2020-08-02 09:40:30 -0700

    Replace navigation panel with tab control.
    Stub partition view model.

---
+++ src/Sciurus/ViewModels/PartitionViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Sciurus.ViewModels
+{
+    /// <summary>
+    /// View model for a workspace partition.
+    /// </summary>
+    public class PartitionViewModel : ViewModel
+    {
+        private string _name;
+
+        /// <summary>
+        /// Intializes a new <see cref="PartitionViewModel"/> instance.
+        /// </summary>
+        public PartitionViewModel()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the partition name.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return this.Get(ref _name);
+            }
+
+            set
+            {
+                this.Set(ref _name, value);
+            }
+        }
+    }
+}
--- src/Sciurus/ViewModels/ShellViewModel.cs
+++ src/Sciurus/ViewModels/ShellViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Sciurus.ViewModels
+﻿using System.Collections.ObjectModel;
+
+namespace Sciurus.ViewModels
 {
     /// <summary>
     /// View model for the application shell.
@@ -7,17 +9,24 @@
     {
         private string _title;
 
+        private ObservableCollection<PartitionViewModel> _partitions;
+        private PartitionViewModel _selectedPartition;
+
         /// <summary>
         /// Intializes a new <see cref="ShellViewModel"/> instance.
         /// </summary>
         public ShellViewModel()
         {
             // TODO: placeholders
-            this.Title = "Partition - Sciurus";
+            this.Title = "Workspace - Sciurus";
+            this.Partitions = new ObservableCollection<PartitionViewModel>();
+            this.Partitions.Add(new PartitionViewModel() { Name = "Default" });
+            this.Partitions.Add(new PartitionViewModel() { Name = "Partition 1" });
+            this.SelectedPartition = this.Partitions[0];
         }
 
         /// <summary>
-        /// Gets or sets the window's title.
+        /// Gets the window's title.
         /// </summary>
         public string Title
         {
@@ -29,6 +38,38 @@
             private set
             {
                 this.Set(ref _title, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets the collection of workspace partitions.
+        /// </summary>
+        public ObservableCollection<PartitionViewModel> Partitions
+        {
+            get
+            {
+                return this.Get(ref _partitions);
+            }
+
+            private set
+            {
+                this.Set(ref _partitions, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the selected partition.
+        /// </summary>
+        public PartitionViewModel SelectedPartition
+        {
+            get
+            {
+                return this.Get(ref _selectedPartition);
+            }
+
+            set
+            {
+                this.Set(ref _selectedPartition, value);
             }
         }
     }
