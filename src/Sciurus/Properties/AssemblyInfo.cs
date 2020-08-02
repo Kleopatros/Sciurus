08c2a581b548ca810f4ed3add3e591b47855f747
Kleopatros <47168369+Kleopatros@users.noreply.github.com>
2020-08-02 11:06:48 -0700

    Fix and cleanup assembly file.

--- src/Sciurus/Properties/AssemblyInfo.cs
+++ src/Sciurus/Properties/AssemblyInfo.cs
@@ -3,9 +3,6 @@ using System.Resources;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Sciurus")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -13,28 +10,13 @@ using System.Windows;
 [assembly: AssemblyProduct("Sciurus")]
 [assembly: AssemblyCopyright("Copyright © Kleopatros 2020")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en-US")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// In order to begin building localizable applications, set
-// <UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-// inside a <PropertyGroup>.  For example, if you are using US english
-// in your source files, set the <UICulture> to en-US.  Then uncomment
-// the NeutralResourceLanguage attribute below.  Update the "en-US" in
-// the line below to match the UICulture setting in the project file.
+// Must mirror the <UICulture>en-US</UICulture> property in ther project file
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-// None: where theme specific resource dictionaries are located
-// (used if a resource is not found in the page,
-// or application resource dictionaries.
-//
-// SourceAssembly: where the generic resource dictionary is
-// located (used if a resource is not found in the page,
-// app, or any theme specific resource dictionaries).
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None,
     ResourceDictionaryLocation.SourceAssembly)]
